aff0341a9e8badd22b13ac80dc94c31a8f1a9c42
ShinoharaTabito <64075243+ShinoharaTabito@users.noreply.github.com>
2020-05-25 15:24:16 +0900

    各ページリンク貼り付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_120555) do
+ActiveRecord::Schema.define(version: 2020_05_25_023756) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ancestry", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
