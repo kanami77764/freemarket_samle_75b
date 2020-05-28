fb6b4c9693d9471247d8c3d39efe87bac80fbf09
ShinoharaTabito <64075243+ShinoharaTabito@users.noreply.github.com>
2020-05-28 16:12:12 +0900

    product_list_commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,22 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all.order('id ASC').limit(3)
+    # @item_imgs = ItemImg.all
   end
 
   def new 
   end
 
   def show
+    @items = Item.find(params[:id])
+    # @item_imgs = ItemImg.find(params[:id])
+    # @item_imgs = @items.item_imgs
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price)
   end
 end
+
+
