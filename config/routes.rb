6dc47b18862d49b073b00912a090054affe3891b
Tomokazu-Kirihara <miw55kzts@icloud.com>
2020-05-30 14:42:38 +0900

    user function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+  
   resources :users, only: [:index, :new]
   resources :items, only: [:show, :new]
   resources :buyers, only: :index
