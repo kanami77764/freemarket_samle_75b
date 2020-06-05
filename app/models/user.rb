4eb5f483571475cd11f73d5220565cb51caf7469
ika1212 <rika.pear06@gmail.comgit config --global user.name ika1212git config --global user.email rika.pear06@gmail.com>
2020-06-05 17:57:15 +0900

    新規登録　バリデーション 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 7 }
   has_many :items
   has_one :credit_card, dependent: :destroy
  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
+  
   has_one :profile, dependent: :destroy
   has_one :sending_destination, dependent: :destroy
 end
