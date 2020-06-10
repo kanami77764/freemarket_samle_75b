797a24fb7a829753c305da2af44477248bab1d15
kanami77764 <64050565+kanami77764@users.noreply.github.com>
2020-06-10 13:05:34 +0900

    comment_test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  validates :comment, presence:true,
+                      length: { minimum: 2, maximum: 100 }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
+describe Comment do
+  
+  describe '#create' do
+    it "user_id、item_id、コメントが存在すれば登録できること" do
+      user = create(:user)
+      item = create(:item)
+      comment = FactoryBot.build(:comment, user_id: user[:id], item_id: item[:id])
+      expect(comment).to be_valid
+    end
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "user_idがない場合は登録できないこと" do
+      comment = build(:comment, user_id: nil)
+      comment.valid?
+      expect(comment.errors[:user]).to include("を入力してください")
+    end
+
+    it "item_idがない場合は登録できないこと" do
+      comment = build(:comment,item_id: nil)
+      comment.valid?
+      expect(comment.errors[:item]).to include("を入力してください")
+    end
+
+    it "コメントがない場合は登録できないこと" do
+      comment = build(:comment, comment: nil)
+      comment.valid?
+      expect(comment.errors[:comment]).to include("を入力してください")
+    end
+
+    it "コメントが101文字以上の場合は登録できないこと" do
+      comment = build(:comment, comment: "a"*101 )
+      comment.valid?
+      expect(comment.errors[:comment]).to include("は100文字以内で入力してください")
+    end
+
+    it "コメントが2文字以下の場合は登録できないこと" do
+      comment = build(:comment, comment: "aa" )
+      comment.valid?
+      expect(comment.errors[:comment]).to include("は3文字以上で入力してください")
+    end
+  end
 end
