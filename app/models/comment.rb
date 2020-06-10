5308d5ed985443a94d012be33684ac6c8fa31f70
kanami77764 <64050565+kanami77764@users.noreply.github.com>
2020-06-10 17:18:52 +0900

    fix_comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :item
 
   validates :comment, presence: true,
-                      length: { minimum: 2, maximum: 100 },
-                      format: {
-                      message: "は3文字以上で入力してください"
-                      }
+                      length: { minimum: 2, maximum: 100 ,message: "は3文字以上で入力してください"}
 end
