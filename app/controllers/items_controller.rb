7ff9412f8804b92902646f08ee67465bd807e30e
sato5extreme <sato5extreme@gmail.com>
2020-06-10 14:11:45 +0900

    感嘆符の削除(create)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save!
+    if @item.save
       redirect_to root_path
     else
       @item.item_imgs.build
